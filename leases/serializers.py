156961bd4d282258f9758e4c8b5f93823ea1456c
kevin <kevinkaranja098@gmail.com>
2026-06-24 10:45:27 +0300

    push for anywhere

--- leases/serializers.py
+++ leases/serializers.py
@@ -18,3 +18,44 @@ class LeaseRequestSerializer(
             "required_amount",
             "status",
         )
+
+class LeaseSerializer(
+    serializers.ModelSerializer
+):
+
+    class Meta:
+
+        model = Lease
+
+        fields = "__all__"
+
+class PaymentSerializer(
+    serializers.ModelSerializer
+):
+
+    class Meta:
+
+        model = Payment
+
+        fields = "__all__"
+
+class LeaseWalletSerializer(
+    serializers.ModelSerializer
+):
+
+    class Meta:
+
+        model = LeaseWallet
+
+        fields = "__all__"
+
+class RentChargeSerializer(
+    serializers.ModelSerializer
+):
+
+    class Meta:
+
+        model = RentCharge
+
+        fields = "__all__"
+
